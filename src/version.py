0c84c4fa4d1f6c9ec458d4399fd253425282174a
miffon <hou1830@gmail.com>
2026-08-22 06:41:21 +0800

    更新套件

--- src/version.py
+++ src/version.py
@@ -2,7 +2,7 @@
 
 from release_config import IS_TEST_BUILD
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 
 def display_version(version: str = __version__, is_test_build: bool = IS_TEST_BUILD) -> str:
